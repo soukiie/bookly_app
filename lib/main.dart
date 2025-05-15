3080d509e0890d26bd7c3017fdaebd01cebc09b7
Sayed Saad <elsayed.saad2014@feps.edu.eg>
2025-05-15 15:59:28 +0300

    Splash screen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: SplashView(),
+    );
+  }
+}
